a0ad19aa9c7f12d3bb95a8a2b719fb37e2231b9c
Karthi <karthi.sekar@gmail.com>
2019-10-19 13:38:54 +0530

    SkillMatrix - API initial setup with EF

---
+++ skill-matrix/api/skill-matrix-api/Repositories/SkillMatrixRepository.cs
@@ -0,0 +1,20 @@
+﻿using skill_matrix_api.Models;
+using System.Linq;
+
+namespace skill_matrix_api.Repositories
+{
+    public class SkillMatrixRepository
+    {
+        SkillMatrixEntities _skillMatrix;
+
+        public SkillMatrixRepository()
+        {
+            _skillMatrix = new SkillMatrixEntities();
+        }
+
+        public void GetEmployee(string employeeId)
+        {
+            _skillMatrix.Employees.FirstOrDefault(x => x.EmployeeId == employeeId);
+        }
+    }
+}
